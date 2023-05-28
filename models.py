74230de5d9a9beb9d621b3947e8f176d2a1783ef
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2023-05-27 23:40:23 -0400

    Add new class based approach

--- models.py
+++ models.py
@@ -1,10 +1,84 @@
-class Recipe:
-    def __init__(self, name:str=None, output_per_minute:int=None, ingredients:list=None):
-        self.name = name
-        self.output_per_minute = output_per_minute
-        self.ingredients = ingredients
+import abc
+from typing import Dict, List, Type
 
-class Item:
-    def __init__(self, name:str=None, recipes:list=None):
-        self.name = name
-        self.recipes = recipes if recipes is not None else list()
+class Recipe(abc.ABC):
+    __default_recipe__: bool = False
+    __itemname__: str
+    __recipename__: str
+    __output_per_minute__: int | None
+    __ingredients__: Dict[str, int] | None
+
+    @classmethod
+    def itemname(cls):
+        return cls.__itemname__
+
+    @classmethod
+    def recipename(cls):
+        return cls.__recipename__
+
+    @classmethod
+    def output_per_minute(cls):
+        return cls.__output_per_minute__
+
+    @classmethod
+    def ingredients(cls):
+        return cls.__ingredients__
+
+
+class IronOre(Recipe):
+    __recipename__ = "Iron Ore"
+    __itemname__ = "Iron Ore"
+    __ingredients__ = None
+    __output_per_minute__ = None
+
+
+class CopperOre(Recipe):
+    __recipename__ = "Copper Ore"
+    __itemname__ = "Copper Ore"
+    __ingredients__ = None
+    __output_per_minute__ = None
+
+
+class IronIngot(Recipe):
+    __recipename__ = "Iron Ingot"
+    __itemname__ = "Iron Ingot"
+    __ingredients__ = {"Iron Ore": 30}
+    __output_per_minute__ = 30
+
+
+class CopperIngot(Recipe):
+    __recipename__ = "Copper Ingot"
+    __itemname__ = "Copper Ingot"
+    __ingredients__ = {"Copper Ore": 30}
+    __output_per_minute__ = 30
+
+
+class Water(Recipe):
+    __recipename__ = "Water"
+    __itemname__ = "Water"
+    __ingredients__ = None
+    __output_per_minute__ = None
+
+
+class PureIronIngot(Recipe):
+    __recipename__ = "Pure Iron Ingot"
+    __itemname__ = "Iron Ingot"
+    __ingredients__ = {"Iron Ore": 35, "Water": 20}
+    __output_per_minute__ = 50
+
+
+RecipeRegistry = Dict[str, List[Type[Recipe]]]
+
+
+def _create_recipe_registry() -> RecipeRegistry:
+    g = {k: v for k, v in globals().items()}
+    r: RecipeRegistry = {}
+    for v in g.values():
+        if isinstance(v, type) and issubclass(v, Recipe) and v.__name__ != Recipe.__name__:
+            if v.__itemname__ not in r:
+                r[v.__itemname__] = [v]
+            else :
+                r[v.__itemname__].append(v)
+    return r
+
+recipe_registry = _create_recipe_registry()
