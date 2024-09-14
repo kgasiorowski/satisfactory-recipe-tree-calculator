3057bb17b40e820a335710579ec65888db11e32f
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2024-09-14 17:42:38 -0400

    Add proof of concept for model rework

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from models import Recipe, Item
+from _legacy_models import Item, Recipe
+import models
 from pprint import pprint
 import json
 
@@ -25,6 +26,11 @@ def load_recipes():
                     newRecipe.name = recipeName
                     newRecipe.output_per_minute = recipeData["output_per_minute"]
                     newRecipe.ingredients = recipeData["inputs"]
+                    newRecipe.machine = (
+                        recipeData["machine"] if "machine" in recipeData else "machine"
+                    )
+                    if "byproduct" in recipeData:
+                        newRecipe.byproduct = list(recipeData["byproduct"].items())[0]
                     newItem.recipes.append(newRecipe)
             flat_recipe_map[newItem.name] = newItem
 
@@ -50,29 +56,56 @@ def load_recipes():
 
 
 def print_recipe(
-    item: Item, amount_per_minute: float, alts: dict = None, skipped_items: list = None
+        item: Item, amount_per_minute: float, alts: dict = None, skipped_items: list = None
 ):
-    alts = alts if alts is not None else load_alt_selections()
+    # alts = alts if alts is not None else load_alt_selections()
+    alts = {**load_alt_selections(), **alts}
     base_ingredient_amounts = {}
+
+    power_dict = {
+        "Assemblers": 15,
+        "Smelters": 4,
+        "Constructors": 4,
+        "Refineries": 30,
+        "Manufacturers": 55,
+        "Foundries": 16,
+        "Particle Accelerators": 1000,
+        "Blenders": 75,
+    }
+    total_power_consumption = 0
 
     def __print_recipe_recurs(item: Item, amount_per_minute: float, tabnum: int):
         tabstring = "".join(["\t" for _ in range(tabnum)])
-        print(f"{tabstring}{item.name} - {round(amount_per_minute, 4)} pm")
+        strang = f"{tabstring}{item.name} - {round(amount_per_minute, 4)} pm"
+
+        recipe = None
+        if item.recipes:
+            recipe = item.recipes["default"]
+
+            if item.name in alts:
+                recipe = item.recipes[alts[item.name]]
+
+            strang += f" - {round(amount_per_minute / recipe.output_per_minute, 2)} {recipe.machine}"
+
+            if recipe.machine in power_dict:
+                nonlocal total_power_consumption
+                total_power_consumption += (
+                        round(amount_per_minute / recipe.output_per_minute, 2)
+                        * power_dict[recipe.machine]
+                )
+
+        print(strang)
 
         if skipped_items is not None and item.name in skipped_items:
             base_ingredient_amounts.setdefault(item.name, 0)
             base_ingredient_amounts[item.name] += amount_per_minute
             return
 
-        if item.recipes:
-            recipe = item.recipes["default"]
-
-            if item.name in alts:
-                recipe = item.recipes[alts[item.name]]
+        if recipe is not None:
             for ingredient in recipe.ingredients:
                 __print_recipe_recurs(
                     ingredient[0],
-                    amount_per_minute * (ingredient[1] / recipe.output_per_minute),
+                    (amount_per_minute * (ingredient[1] / recipe.output_per_minute)),
                     tabnum + 1,
                 )
         else:
@@ -82,8 +115,8 @@ def print_recipe(
     __print_recipe_recurs(item, amount_per_minute, 0)
 
     base_ingredient_amounts = {
-        key: round(base_ingredient_amounts[key], 4)
-        for key, value in base_ingredient_amounts.items()
+        **{key: round(value, 4) for key, value in base_ingredient_amounts.items()},
+        "total_power_consumption": total_power_consumption,
     }
 
     return base_ingredient_amounts
@@ -92,13 +125,16 @@ def print_recipe(
 recipes = load_recipes()
 
 if __name__ == "__main__":
-    pprint(
-        print_recipe(
-            recipes["High-Speed Connector"],
-            40,
-            alts={
-                "High-Speed Connector": "Silicon High-Speed Connector",
-                "Circuit Board": "Silicon Circuit Board",
-            },
-        )
-    )
+    # pprint(
+    #     print_recipe(
+    #         recipes["High-Speed Connector"],
+    #         40,
+    #         alts={
+    #             "High-Speed Connector": "Silicon High-Speed Connector",
+    #             "Circuit Board": "Silicon Circuit Board",
+    #         },
+    #     )
+    # )
+
+    pprint(models.iron_ingot.model_dump())
+    ...
--- models.py
+++ models.py
@@ -1,10 +1,49 @@
-class Recipe:
-    def __init__(self, name:str=None, output_per_minute:int=None, ingredients:list=None):
-        self.name = name
-        self.output_per_minute = output_per_minute
-        self.ingredients = ingredients
+import pydantic
+import abc
+import enum
 
-class Item:
-    def __init__(self, name:str=None, recipes:list=None):
-        self.name = name
-        self.recipes = recipes if recipes is not None else list()
+
+class BaseModel(pydantic.BaseModel, abc.ABC):
+    ...
+
+
+class MachineType(enum.Enum):
+    constructor = 4
+    assembler = 15
+    smelter = 4
+    foundry = 16
+    refinery = 30
+
+
+class Recipe(BaseModel):
+    name: str
+    output_per_minute: int
+    ingredients: list[tuple["Item", int]]
+    machine: MachineType
+    byproduct: tuple[str, int] | None = None
+
+
+class Item(BaseModel):
+    name: str = "default"
+    recipes: list[Recipe] | None = None
+
+
+water = Item(name="Water")
+iron_ore = Item(name="Iron Ore")
+iron_ingot = Item(
+    name="Iron Ingot",
+    recipes=[
+        Recipe(
+            name="default",
+            output_per_minute=30,
+            ingredients=[(iron_ore, 30)],
+            machine=MachineType.smelter,
+        ),
+        Recipe(
+            name="Pure Iron Ingot",
+            output_per_minute=65,
+            ingredients=[(iron_ore, 35), (water, 20)],
+            machine=MachineType.refinery,
+        ),
+    ],
+)
